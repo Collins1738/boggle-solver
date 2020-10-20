bbc114b7236c7d961debeadf20fe2cfbf2e750b5
Collins1738 <tobechikeluba@gmail.com>
2020-10-20 00:16:07 -0400

    added start stop buttons

--- src/App.js
+++ src/App.js
@@ -39,22 +39,26 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			grid: grid,
+			grid: [],
 			dictionary: dictionary,
 			solutions: [],
 			value: "",
 			found: [],
 			prompt: "",
+			gameEnded: false,
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.startGame = this.startGame.bind(this);
+		this.stopGame = this.stopGame.bind(this);
 	}
 
 	render() {
 		const { value, prompt } = this.state;
 		return (
-			<div>
+			<div align="center">
 				<h2>Boggle Solver</h2>
+				{this.startStopButtons()}
 				{this.renderButtonsGrid()}
 				<form>
 					<input
@@ -71,9 +75,19 @@ class App extends Component {
 				<div>{prompt}</div>
 
 				{this.foundWords()}
+				{this.remainingWords()}
 			</div>
 		);
 	}
+
+	startStopButtons = () => {
+		return (
+			<div>
+				<button onClick={this.startGame}>Start</button>
+				<button onClick={this.stopGame}>Stop</button>
+			</div>
+		);
+	};
 
 	renderButtonsGrid = () => {
 		const { grid } = this.state;
@@ -97,18 +111,26 @@ class App extends Component {
 	};
 
 	foundWords = () => {
-		const { found } = this.state;
+		const { found, gameEnded } = this.state;
 		var foundWords = found.map((word) => {
 			return <div>{word}</div>;
 		});
 		return <div>Found Words: {foundWords}</div>;
 	};
 
-	componentDidMount() {
-		const { grid, dictionary } = this.state;
-		const solutions = findAllSolutions(grid, dictionary);
-		this.setState({ solutions });
-	}
+	remainingWords = () => {
+		const { gameEnded, solutions, found } = this.state;
+		var remainingWords = solutions.map((word) => {
+			if (!found.includes(word)) {
+				return <div>{word}</div>;
+			}
+		});
+		return gameEnded ? (
+			<div>Remaining Words: {remainingWords}</div>
+		) : (
+			<div></div>
+		);
+	};
 
 	handleChange(event) {
 		this.setState({ [event.target.id]: event.target.value });
@@ -129,6 +151,16 @@ class App extends Component {
 			this.setState({ prompt: `${value} is wrong` });
 		}
 		this.setState({ value: "" });
+	}
+
+	startGame() {
+		const { dictionary } = this.state;
+		this.setState({ grid });
+		const solutions = findAllSolutions(grid, dictionary);
+		this.setState({ solutions });
+	}
+	stopGame() {
+		this.setState({ gameEnded: true });
 	}
 }
 
